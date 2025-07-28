a440ceba4988ff9a81cef7edbf0c4e0266525629
Valentina <20210689@ricaldoneedu.sv>
2025-07-28 10:43:41 -0600

    Carga inicial del proyecto

---
+++ ProyectoModulo/src/main/java/ValentinaDale/ProyectoModulo/ProyectoModuloApplication.java
@@ -0,0 +1,13 @@
+package ValentinaDale.ProyectoModulo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoModuloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoModuloApplication.class, args);
+	}
+
+}
---
+++ ProyectoModulo/src/test/java/ValentinaDale/ProyectoModulo/ProyectoModuloApplicationTests.java
@@ -0,0 +1,13 @@
+package ValentinaDale.ProyectoModulo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoModuloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
